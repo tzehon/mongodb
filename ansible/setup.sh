54908c1cf55c90ca5f75ef9ed378ad597c6b3262
Tan Tze Hon <tze hon>
2024-07-03 20:05:40 +0800

    Disable thp

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -5,10 +5,11 @@ set -e
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
+ansible-playbook -i hosts disable_thp.yml --ask-become-pass
 ansible-playbook -i hosts mongodb_enterprise_replica_set_setup.yml --ask-become-pass
 
 echo "Replica set setup successfully."
 
-mongorestore --uri="mongodb://mongoAdmin:Password123@192.168.64.5:27017,192.168.64.6:27017,192.168.64.7:27017/?replicaSet=rs0&appName=mongosh+2.2.10" --dir=dump
+mongorestore --uri="mongodb://mongoAdmin:Password123@192.168.64.5:27017,192.168.64.6:27017,192.168.64.7:27017/?replicaSet=rs0" --dir=dump
 
 echo "Data loaded successfully."
